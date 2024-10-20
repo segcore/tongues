be54cf48d528868910e3a92858b91acb71f52a1f
Nicholas Rixson <44293604+segcore@users.noreply.github.com>
2024-10-20 16:01:04 +0800

    Add a build script to build all items

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+build() {
+    (
+        cd "$1"
+        echo "=== $1   $2"
+        time $2
+    )
+}
+build c "cc cat.c -o cat"
+build c++ "g++ cat.cpp -o cat"
+build csharp "mcs cat.cs -out:cat"
+build haskell "ghc cat.hs"
+build lua "luac -o cat cat.lua" # Lua is not usually compiled, but it can be to bytecode
+build odin "odin build cat.odin -file"
+build rust "rustc cat.rs"
+build zig "zig build-exe cat.zig"
