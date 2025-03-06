7be06eab01d0bbb6b374e6f4636fc0178fdbb931
segcore <segcore@github.com>
2025-03-06 23:11:46 +0800

    Add jai cat

--- build.sh
+++ build.sh
@@ -7,10 +7,12 @@ build() {
         time $2
     )
 }
+
 build c "cc cat.c -o cat"
 build c++ "g++ cat.cpp -o cat"
 build csharp "mcs cat.cs -out:cat"
 build haskell "ghc cat.hs"
+build jai "jai cat.jai -quiet"
 build lua "luac -o cat cat.lua" # Lua is not usually compiled, but it can be to bytecode
 build odin "odin build cat.odin -file"
 build rust "rustc cat.rs"
